b0d860d2d2c4ef2c207109677fdcf3c46abbefe3
Carlos Grék <grekcarlos@gmail.com>
2022-10-18 12:52:16 +0200

    Day 3 - Rollercoaster Challenge Upgrade

--- Day3-ControlFlow/main.py
+++ Day3-ControlFlow/main.py
@@ -5,11 +5,23 @@ if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        print("Child tickets are $5.")
+        total_cost = 5
     elif age <= 18:
-        print("Please pay $7.")
+        print("Youth tickets are $7.")
+        total_cost = 7
     else:
-        print("Please pay $12.")
+        print("Adult tickets are $12.")
+        total_cost = 12
+
+    wants_photo = input("Do you want a photo taken fro $10? Yes or No? ")
+    if wants_photo == "Yes":
+        total_cost += 10
+        print(f"Your total cost comes to ${total_cost}.")
+
+    else:
+        print(f"Your total cost comes to ${total_cost}.")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
